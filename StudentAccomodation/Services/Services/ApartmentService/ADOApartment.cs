944933877784740bac8c92f0bfc19e9b7cfc1d08
Jakub Cerovský <jaku1198@edu.easj.dk>
2022-03-23 20:37:52 +0100

    Jen zaklady

---
+++ StudentAccomodation/Services/Services/ApartmentService/ADOApartment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAccomodation.Services.Services.ApartmentService
+{
+    public class ADOApartment
+    {
+    }
+}
